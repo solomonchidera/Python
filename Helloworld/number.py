75b2033245fd934e6861cf3eaa513d1c6183e9ff
SolomonChidera <solochidera10@gmail.com>
2023-12-30 09:12:52 +0100

    Practicing from scratch

---
+++ Helloworld/number.py
@@ -0,0 +1,11 @@
+num1, num2 = input("Enter two number for calculations ").split()
+num1 = int(num1)
+num2 = int(num2)
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotent = num1 / num2
+div = num1 // num2
+
+print("{} + {} = {}".format(num1, num2, sum))
