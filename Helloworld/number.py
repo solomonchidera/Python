59d574b68c09bec11df135217d2dad12f7bb4c82
SolomonChidera <solochidera10@gmail.com>
2023-12-30 10:55:16 +0100

    Practicing from scratch

--- Helloworld/number.py
+++ Helloworld/number.py
@@ -7,9 +7,11 @@ difference = num1 - num2
 product = num1 * num2
 quotent = num1 / num2
 div = num1 // num2
+remainder = num1 % num2
 
 print("{} + {} = {}".format(num1, num2, sum))
 print("{} - {} = {}".format(num1, num2, difference))
 print("{} * {} = {}".format(num1, num2, product))
 print("{} / {} = {}".format(num1, num2, quotent))
 print("{} // {} = {}".format(num1, num2, div))
+print("{} % {} = {}".format(num1, num2, remainder))
