8cab8a6081ca9f00b27f5f1f6e2f8baa136c51a7
SolomonChidera <solochidera10@gmail.com>
2024-01-07 15:22:37 +0100

    This year is about getting things done

---
+++ Helloworld/cond.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+for i in range(1, 10):
+    if i <= 5:
+        print("AAAAAAAHHHH")
+    elif i >= 6 and i <= 8:
+        print("Where are all the brackets")
+    else:
+        print("How do you people read this syntax")
